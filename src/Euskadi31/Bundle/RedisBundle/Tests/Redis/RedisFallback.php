29a1003aa47e871311ca2479dc0627a764b7eb19
Axel Etcheverry <euskadi31@gmail.com>
2015-03-12 10:52:06 +0100

    chore(src): add default comments with author and license

--- src/Euskadi31/Bundle/RedisBundle/Tests/Redis/RedisFallback.php
+++ src/Euskadi31/Bundle/RedisBundle/Tests/Redis/RedisFallback.php
@@ -1,4 +1,12 @@
 <?php
+/*
+ * This file is part of the RedisBundle package.
+ *
+ * (c) Axel Etcheverry <axel@etcheverry.biz>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
 class RedisMasterDiscovery
 {
