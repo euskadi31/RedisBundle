08f2cfad4f187ad7ceb0580b0a5548d1e8f78d8b
Axel Etcheverry <euskadi31@gmail.com>
2015-03-12 14:27:28 +0100

    fix(command): remove unused namespace

--- src/Euskadi31/Bundle/RedisBundle/Command/RedisDebugCommand.php
+++ src/Euskadi31/Bundle/RedisBundle/Command/RedisDebugCommand.php
@@ -13,9 +13,6 @@ namespace Euskadi31\Bundle\RedisBundle\Command;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
-use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\DependencyInjection\Exception\ServiceNotFoundException;
 use Redis;
 
 class RedisDebugCommand extends ContainerAwareCommand
